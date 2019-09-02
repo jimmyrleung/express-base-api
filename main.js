15a90b42a980f5d3c57992f44a0d8a3ff7ef92b0
Jimmy <jimmy.rl19@hotmail.com>
2019-09-02 16:49:13 -0300

    redis module refactor

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
 require('dotenv').config();
 
 const init = require('./src/express'),
-    { register, set, get } = require('./src/util/redis-module');
+    redisModule = require('./src/util/redis-module');
 
-register();
-
-async function testRedisConnection() {
-    try {
-        await set('test-key', 'test');
-
-        const testValue = await get('test-key');
-        console.log(testValue);
-    }
-    catch (ex) {
-        console.log(`Couldn't test redis connection: ${ex}`);
-    }
-}
-
-testRedisConnection();
-
+redisModule.register();
+redisModule.testConnection();
 init();
--- src/util/redis-module.js
+++ src/util/redis-module.js
@@ -3,34 +3,52 @@ const redis = require('redis'),
     REDIS_KEY = Symbol.for(redisConfig.SYMBOL),
     client = redis.createClient({ port: redisConfig.PORT });
 
-module.exports = {
-    register: () => {
-        if (!global[REDIS_KEY]) {
-            global[REDIS_KEY] = {
-                client
-            };
-        }
-    },
-    get: (key) => {
-        return new Promise((resolve, reject) => {
-            const redisClient = global[REDIS_KEY].client;
-
-            redisClient.get(key, (err, value) => {
-                if (err) return reject(err);
-                resolve(value);
-            });
-        });
-    },
-
-    set: (key, value) => {
-        return new Promise((resolve, reject) => {
-            const redisClient = global[REDIS_KEY].client;
-
-            redisClient.set(key, value, (err) => {
-                if (err) return reject(err);
-                resolve();
-            });
-        });
+const register = () => {
+    if (!global[REDIS_KEY]) {
+        global[REDIS_KEY] = {
+            client
+        };
     }
+}
 
+const get = (key) => {
+    return new Promise((resolve, reject) => {
+        const redisClient = global[REDIS_KEY].client;
+
+        redisClient.get(key, (err, value) => {
+            if (err) return reject(err);
+            resolve(value);
+        });
+    });
+}
+
+const set = (key, value) => {
+    return new Promise((resolve, reject) => {
+        const redisClient = global[REDIS_KEY].client;
+
+        redisClient.set(key, value, (err) => {
+            if (err) return reject(err);
+            resolve();
+        });
+    });
+}
+
+const testConnection = async () => {
+    try {
+        await set('test-key', 'test');
+
+        const testValue = await get('test-key');
+        console.log(testValue);
+    }
+    catch (ex) {
+        console.log(`Couldn't test redis connection: ${ex}`);
+        throw ex;
+    }
+}
+
+module.exports = {
+    get,
+    set,
+    register,
+    testConnection,
 };
