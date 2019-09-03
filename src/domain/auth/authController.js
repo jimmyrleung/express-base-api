a65a0390dc456ddb16fc114465d545bf6bbd00df
Jimmy <jimmy.rl19@hotmail.com>
2019-09-03 16:52:58 -0300

    basic login service

--- src/domain/auth/authController.js
+++ src/domain/auth/authController.js
@@ -1,7 +1,23 @@
-const { jwt } = require('../../util');
+const Auth = require('./Auth');
+const authService = require('./authService');
 
-const login = (req, res) => {
-    const token = jwt.generate(req.body);
+const login = async (req, res) => {
+    const credentials = new Auth(req.body);
+
+    if (!await credentials.isValid()) {
+        return res.status(400).json({
+            errors: credentials.errors
+        })
+    }
+
+    const token = await authService.login(credentials.values);
+
+    if (!token) {
+        return res.status(400).json({
+            errors: [`Email or password invalid.`]
+        })
+    }
+
     res.json({ token });
 }
 
---
+++ src/domain/auth/authService.js
@@ -0,0 +1,25 @@
+const { jwt, crypto } = require('../../util');
+const { UserModel } = require('../users');
+
+const login = async (credentials) => {
+    const user =
+        await UserModel.findOne({ email: credentials.email });
+
+    // TODO: Return user not found error
+    if (!user) return null;
+
+    const { hash } = crypto.generateHash(credentials.password, user.salt);
+
+    // TODO: Return incorrect password error
+    if (user.password !== hash) return null;
+
+    const token =
+        jwt.generate({ name: user.name, email: user.email });
+
+    return token;
+
+}
+
+module.exports = {
+    login,
+}
