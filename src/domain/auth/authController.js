80c02561387e6e3f843e259593aff349361b960f
Jimmy <jimmy.rl19@hotmail.com>
2019-09-05 17:55:20 -0300

    node mailer basic config

--- src/domain/auth/authController.js
+++ src/domain/auth/authController.js
@@ -1,6 +1,8 @@
 const Auth = require('./Auth');
+const moment = require('moment');
 const authService = require('./authService');
 const { CustomErrorHandler } = require('../../util');
+const mailer = require('../../mail/mailer');
 
 const login = async (req, res) => {
   const credentials = new Auth(req.body);
@@ -13,7 +15,16 @@ const login = async (req, res) => {
 
   try {
     const token = await authService.login(credentials.values);
-    return res.json({ token });
+    res.json({ token });
+
+    return mailer.send({
+      address: 'jimmy.arfs@gmail.com,jimmy.rl19@hotmail.com',
+      subject: 'New account login',
+      template: 'login',
+    }, {
+      name: 'Jimmy Rios Leung',
+      loginDateString: moment.utc().format('DD/MM/YYYY HH:mm:ss'),
+    });
   } catch (err) {
     req.logger.error('Couldn\'t log the user in.', err);
     return CustomErrorHandler.handle(err, res);
---
+++ src/mail/mailer.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+const nodemailer = require('nodemailer');
+const { logger } = require('../util');
+
+const createTransporter = async () => {
+  // TODO: Create a real mail account
+  const testAccount = await nodemailer.createTestAccount();
+
+  // TODO: Replace with a real SMTP Transport configuration
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  return transporter;
+};
+
+const readTemplate = async (templateName) => {
+  const read = (filePath) => new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, fileAsString) => {
+      if (err) return reject(err);
+      return resolve(fileAsString);
+    })
+  });
+
+  const templatePath = await path.resolve(__dirname, 'templates', `${templateName}.html`);
+  const template = await read(templatePath);
+
+  return template;
+};
+
+const replaceParams = (originalTemplate, data) => {
+  let template = originalTemplate;
+
+  Object.keys(data).forEach((key) => {
+    template = template.replace(`{{${key}}}`, data[key]);
+  });
+
+  return template;
+};
+
+const send = async (mailInfo, data) => {
+  try {
+    const transporter = await createTransporter();
+    const template = replaceParams(await readTemplate(mailInfo.template), data);
+
+    const result = await transporter.sendMail({
+      from: 'Express base api <noreply@expressbaseapi.com>',
+      to: mailInfo.address,
+      subject: mailInfo.subject,
+      html: template,
+    });
+
+    logger.info(`Mail successfully sent to ${mailInfo.address} with the subject ${mailInfo.subject}. Result: ${JSON.stringify(result)}.`);
+  } catch (err) {
+    logger.error(`An error occurred while trying to send the email to ${mailInfo.address} with the subject ${mailInfo.subject}`, err);
+    throw err;
+  }
+};
+
+module.exports = {
+  send,
+};
