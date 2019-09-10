6d09aaece29321e2d7cf8cd0e2e367104e551384
Jimmy <jimmy.rl19@hotmail.com>
2019-09-10 16:22:10 -0300

    include 'Authorization' header on cors

--- src/middlewares/enableCors.js
+++ src/middlewares/enableCors.js
@@ -1,6 +1,6 @@
 const enableCors = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
   if (req.method === 'OPTIONS') {
     return res.json();
