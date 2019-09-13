a372027de2262364594867e4c4b07203ee4b44e8
Jimmy <jimmy.rl19@hotmail.com>
2019-09-13 15:50:18 -0300

    cors methods

--- src/middlewares/enableCors.js
+++ src/middlewares/enableCors.js
@@ -1,5 +1,6 @@
 const enableCors = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
   if (req.method === 'OPTIONS') {
