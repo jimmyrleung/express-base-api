66b80427cef6e20cf49fd9475567980e66c52fbd
Jimmy Leung <jimmy.rl19@hotmail.com>
2020-03-09 11:17:39 -0300

    measure express response time

--- src/express.js
+++ src/express.js
@@ -1,10 +1,16 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const responseTime = require('response-time');
 
-const app = express();
 const { serverConfig } = require('./config');
 const { registerRoutes } = require('./routes');
 const { authentication, loggerLoader, enableCors } = require('./middlewares');
+
+const app = express();
+
+app.use(responseTime((req, res, time) => {
+  console.log(`Response time: ${time}`);
+}));
 
 app.use(bodyParser.json());
 app.use(enableCors);
